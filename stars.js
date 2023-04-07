4b910a9312290ec895bcc186053a3108fe6f3304
Abigail Dillehay <ard99@yahoo.com>
2023-04-07 16:07:20 -0400

    added an additional bootstrap component

--- stars.js
+++ stars.js
@@ -8,3 +8,15 @@ for(let i = 0; i < STAR_COUNT; i++){
     result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
 }
 console.log(result.substring(0, result.length - 1))
+
+// to make the 'toast' feature work:
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    console.log("button clicked")
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
